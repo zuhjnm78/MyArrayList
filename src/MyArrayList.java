b12e028ccfcd7db72be06ef5a5ee74a57e3ac4aa
oleghenkel <oleg.henkel75@gmail.com>
2023-08-16 14:50:44 +0300

    HW MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,174 @@
-public class MyArrayList implements StringList{
+import java.util.Arrays;
 
-}
+    public class MyArrayList implements StringList {
+
+        private static final int DEFAULT_CAPACITY = 10;
+
+        private String[] array;
+        private int size;
+
+        public MyArrayList() {
+            array = new String[DEFAULT_CAPACITY];
+            size = 0;
+        }
+
+        public MyArrayList(int initialCapacity) {
+            if (initialCapacity < 0) {
+                throw new IllegalArgumentException("Initial capacity cannot be negative");
+            }
+            array = new String[initialCapacity];
+            size = 0;
+        }
+
+        private void ensureCapacity(int minCapacity) {
+            int oldCapacity = array.length;
+            if (minCapacity > oldCapacity) {
+                int newCapacity = Math.max(oldCapacity * 2, minCapacity);
+                array = Arrays.copyOf(array, newCapacity);
+            }
+        }
+
+        @Override
+        public String add(String item) {
+            if (item == null) {
+                throw new IllegalArgumentException("Item cannot be null");
+            }
+            ensureCapacity(size + 1);
+            array[size] = item;
+            size++;
+            return item;
+        }
+        @Override
+        public String add(int index, String item) {
+            if (item == null) {
+                throw new IllegalArgumentException("Item cannot be null");
+            }
+            if (index < 0 || index > size) {
+                throw new IndexOutOfBoundsException("Index is out of bounds");
+            }
+            ensureCapacity(size + 1);
+            System.arraycopy(array, index, array, index + 1, size - index);
+            array[index] = item;
+            size++;
+            return item;
+        }
+
+        @Override
+        public String set(int index, String item) {
+            if (item == null) {
+                throw new IllegalArgumentException("Item cannot be null");
+            }
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Index is out of bounds");
+            }
+            String oldItem = array[index];
+            array[index] = item;
+            return oldItem;
+        }
+
+        @Override
+        public String remove(String item) {
+            int index = indexOf(item);
+            if (index != -1) {
+                return remove(index);
+            }
+            throw new IllegalArgumentException("Item not found");
+        }
+
+        @Override
+        public String remove(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Index is out of bounds");
+            }
+            String removedItem = array[index];
+            System.arraycopy(array, index + 1, array, index, size - index - 1);
+            array[size - 1] = null; // Clear the last element
+            size--;
+            return removedItem;
+        }
+        @Override
+        public boolean contains(String item) {
+            if (item == null) {
+                return false;
+            }
+            for (int i = 0; i < size; i++) {
+                if (item.equals(array[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public int indexOf(String item) {
+            if (item == null) {
+                return -1;
+            }
+            for (int i = 0; i < size; i++) {
+                if (item.equals(array[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        @Override
+        public int lastIndexOf(String item) {
+            if (item == null) {
+                return -1;
+            }
+            for (int i = size - 1; i >= 0; i--) {
+                if (item.equals(array[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        @Override
+        public String get(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Index is out of bounds");
+            }
+            return array[index];
+        }
+
+        @Override
+        public boolean equals(StringList otherList) {
+            if (otherList == null) {
+                throw new IllegalArgumentException("Other list cannot be null");
+            }
+            if (this == otherList) {
+                return true;
+            }
+            if (this.size() != otherList.size()) {
+                return false;
+            }
+            for (int i = 0; i < size; i++) {
+                if (!this.get(i).equals(otherList.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public void clear() {
+            Arrays.fill(array, 0, size, null);
+            size = 0;
+        }
+
+        @Override
+        public String[] toArray() {
+            return Arrays.copyOf(array, size);
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+    }
--- src/StringList.java
+++ src/StringList.java
@@ -1,80 +1,30 @@
 public interface StringList {
-    public interface StringList {
+    String add(String item);
 
-        // Добавление элемента.
-        // Вернуть добавленный элемент
-        // в качестве результата выполнения.
-        String add(String item);
+    String add(int index, String item);
 
-        // Добавление элемента
-        // на определенную позицию списка.
-        // Если выходит за пределы фактического
-        // количества элементов или массива,
-        // выбросить исключение.
-        // Вернуть добавленный элемент
-        // в качестве результата выполнения.
-        String add(int index, String item);
+    String set(int index, String item);
 
-        // Установить элемент
-        // на определенную позицию,
-        // затерев существующий.
-        // Выбросить исключение,
-        // если индекс больше
-        // фактического количества элементов
-        // или выходит за пределы массива.
-        String set(int index, String item);
+    String remove(String item);
 
-        // Удаление элемента.
-        // Вернуть удаленный элемент
-        // или исключение, если подобный
-        // элемент отсутствует в списке.
-        String remove(String item);
+    String remove(int index);
+    boolean contains(String item);
 
-        // Удаление элемента по индексу.
-        // Вернуть удаленный элемент
-        // или исключение, если подобный
-        // элемент отсутствует в списке.
-        String remove(int index);
 
-        // Проверка на существование элемента.
-        // Вернуть true/false;
-        boolean contains(String item);
+    int indexOf(String item);
 
-        // Поиск элемента.
-        // Вернуть индекс элемента
-        // или -1 в случае отсутствия.
-        int indexOf(String item);
+    int lastIndexOf(String item);
 
-        // Поиск элемента с конца.
-        // Вернуть индекс элемента
-        // или -1 в случае отсутствия.
-        int lastIndexOf(String item);
+    String get(int index);
 
-        // Получить элемент по индексу.
-        // Вернуть элемент или исключение,
-        // если выходит за рамки фактического
-        // количества элементов.
-        String get(int index);
+    boolean equals(StringList otherList);
 
-        // Сравнить текущий список с другим.
-        // Вернуть true/false или исключение,
-        // если передан null.
-        boolean equals(StringList otherList);
+    boolean isEmpty();
 
-        // Вернуть фактическое количество элементов.
-        int size();
+    void clear();
 
-        // Вернуть true,
-        // если элементов в списке нет,
-        // иначе false.
-        boolean isEmpty();
+    String[] toArray();
 
-        // Удалить все элементы из списка.
-        void clear();
 
-        // Создать новый массив
-        // из строк в списке
-        // и вернуть его.
-        String[] toArray();
-    }
+    int size();
 }
